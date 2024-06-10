f2186d55bec526246bcc58b7eba4e0b28b5cba97
Vishal Vadhel <vishalvadhel2001@gmail.com>
2024-06-10 19:44:44 +0100

    Add files via upload

---
+++ IOT_Dashboard/my_chart.js
@@ -0,0 +1,20 @@
+const ctx = document.getElementById('myChart1');
+
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: 'data',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
